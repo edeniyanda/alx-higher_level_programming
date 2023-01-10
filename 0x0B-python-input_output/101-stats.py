ba1a7390d5e87d39c0e45a6a28ceb1a075f4e33f
Eden <edeniyanda@gmail.com>
2023-01-10 10:00:52 -0800

    101-stats.py commit

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+
+def print_info():
+    print('File size: {:d}'.format(file_size))
+
+    for scode, code_times in sorted(status_codes.items()):
+        if code_times > 0:
+            print('{}: {:d}'.format(scode, code_times))
+
+
+status_codes = {
+    '200': 0,
+    '301': 0,
+
